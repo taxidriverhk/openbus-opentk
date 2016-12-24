d04b7fbddb1c30f6d4a72a792a2a5e865f157ef8
Alex Leung <taxidriverhk@gmail.com>
2016-12-23 23:13:17 -0800

    MODIFY: Made logging to a file done in a separate thread.

--- OpenBus.Common/Log.cs
+++ OpenBus.Common/Log.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace OpenBus.Common
 {
@@ -17,21 +18,48 @@ namespace OpenBus.Common
 
     public static class Log
     {
+        private const int LOG_LIST_THRESHOLD = 100;
+        private const string LOG_FILE_FORMAT = "OpenBDS_Log_{0}.txt";
+        private const string LOG_LINE_FORMAT = "{0} - {1} - {2}";
+
+        private static List<string> logs;
+        private static Thread logWriteThread;
+
         private static LogLevel lowestLevel;
         private static string logFilePath;
-        private static readonly string logLineFormat = "{0} - {1} - {2}{3}";
 
         static Log()
         {
+            lowestLevel = LogLevel.Debug;
             logFilePath = EnvironmentVariables.RootPath 
-                + string.Format("OpenBDS_Log_{0}.txt", DateTime.Now.ToString("yyyy-MM-dd"));
+                + string.Format(LOG_FILE_FORMAT, DateTime.Now.ToString("yyyy-MM-dd"));
+            logs = new List<string>();
         }
 
         public static void Write(LogLevel level, string format, params object[] variables)
         {
             string message = string.Format(format, variables);
-            File.AppendAllText(logFilePath, string.Format(logLineFormat, 
-                DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), level.ToString(), message, Environment.NewLine));
+            if (level <= lowestLevel)
+                logs.Add(string.Format(LOG_LINE_FORMAT, 
+                    DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff"), 
+                    level.ToString(), message));
+            if (logs.Count > LOG_LIST_THRESHOLD)
+                RunLogWriteThread();
+        }
+
+        private static void RunLogWriteThread()
+        {
+            if (logWriteThread == null || 
+                logWriteThread.ThreadState != ThreadState.Running)
+            {
+                logWriteThread = new Thread(delegate ()
+                {
+                    File.AppendAllLines(logFilePath, logs);
+                    logs.Clear();
+                });
+                logWriteThread.IsBackground = true;
+                logWriteThread.Start();
+            }
         }
     }
 }

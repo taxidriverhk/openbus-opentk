586c16270e2a047b081b8ec026aed259f987c5d3
Alex Leung <taxidriverhk@gmail.com>
2018-02-04 01:50:43 -0800

    FIX: Added back AssemblyInfo.cs

---
+++ OpenBus.Engine/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+ 
+using System.Reflection; 
+using System.Runtime.CompilerServices; 
+using System.Runtime.InteropServices; 
+ 
+[assembly: AssemblyTitle("OpenBus.Engine")] 
+[assembly: AssemblyDescription("")] 
+[assembly: AssemblyConfiguration("")] 
+[assembly: AssemblyCompany("Alex Leung")] 
+[assembly: AssemblyProduct("Open Bus Driving Simulator")] 
+[assembly: AssemblyCopyright("")] 
+[assembly: AssemblyTrademark("")] 
+[assembly: AssemblyCulture("")] 
+[assembly: ComVisible(false)] 
+[assembly: AssemblyVersion("1.0.0.73")] 
+[assembly: AssemblyFileVersion("1.0.0.73")] 
